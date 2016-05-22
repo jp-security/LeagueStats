7ede9c977949aee07520d5b172825c53f3b548f1
J2security <josh_pachmayer@yahoo.com>
2016-05-22 12:11:02 -0400

    Configured the backref to get games that a team has played in whether it is home or away

--- app/models.py
+++ app/models.py
@@ -7,8 +7,12 @@ class Teams(db.Model):
     team_city = db.Column(db.String(256))
     team_name = db.Column(db.String(256))
 
+    home = db.relationship('Games', lazy='joined', foreign_keys='Games.home_team', backref='home_game')
+    away = db.relationship('Games', lazy='joined', foreign_keys='Games.away_team', backref='away_game')
+
     def __repr__(self):
         return '<%r %r %r>' % (self.id, self.team_city, self.team_name)
+        return '<%r %r>' % (self.home, self.away)
 
 class Games(db.Model):
     __tablename__ = 'games'
@@ -32,7 +36,7 @@ class Games(db.Model):
     away = db.relationship('Teams', lazy='joined', foreign_keys='Games.away_team', backref='game_asaway')
 
     def __repr__(self):
-        return ('<Home Team - %r: Total Yards - %r Passing Yards - %r Rushing Yards - %r Tunrovers - %r QBR - %r'
-                'Away Team - %r: Total Yards - %r Passing Yards - %r Rushing Yards - %r Tunrovers - %r QBR - %r'
+        return ('<Home Team - %r: Total Yards - %r Passing Yards - %r Rushing Yards - %r Tunrovers - %r QBR - %r> '
+                '<Away Team - %r: Total Yards - %r Passing Yards - %r Rushing Yards - %r Tunrovers - %r QBR - %r>'
                 % (self.home, self.home_total_yards, self.home_passing_yards, self.home_rushing_yards, self.home_turn_overs, self.home_qbr
                 , self.away, self.away_total_yards, self.away_passing_yards, self.away_rushing_yards, self.away_turn_overs, self.away_qbr))
