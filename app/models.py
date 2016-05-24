040cd34b1acf82036bfb8ab54855ccf2343e4716
J2security <josh_pachmayer@yahoo.com>
2016-05-24 17:19:40 -0400

    Added Wins/Losses to Teams.  Added week to Games

--- app/models.py
+++ app/models.py
@@ -7,17 +7,20 @@ class Teams(db.Model):
     team_owner = db.Column(db.String(256))
     team_city = db.Column(db.String(256))
     team_name = db.Column(db.String(256))
+    team_wins = db.Column(db.Integer)
+    team_losses = db.Column(db.Integer)
 
     home = db.relationship('Games', lazy='joined', foreign_keys='Games.home_team', backref='home_game')
     away = db.relationship('Games', lazy='joined', foreign_keys='Games.away_team', backref='away_game')
 
     def __repr__(self):
-        return '<%r %r %r>' % (self.id, self.team_city, self.team_name)
-        return '<%r %r>' % (self.home, self.away)
+        return '<%r %r %r %r %r %r>' % (self.id, self.team_owner, self.team_city, self.team_name, self.team_wins, self.team_losses)
+        #return '<%r %r>' % (self.home, self.away)
 
 class Games(db.Model):
     __tablename__ = 'games'
     id = db.Column(db.Integer, primary_key=True)
+    week = db.Column(db.Integer)
 
     home_team = db.Column(db.Integer, db.ForeignKey('teams.id'))
     home_score = db.Column(db.Integer)
