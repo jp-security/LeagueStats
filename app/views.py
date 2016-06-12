3e3675cac15e620e8c51ae70df34e3f94e49a1e8
J2security <josh_pachmayer@yahoo.com>
2016-06-12 11:46:46 -0400

    Finished moving over to sortable tables

--- app/views.py
+++ app/views.py
@@ -100,13 +100,6 @@ def week8():
                             active_week8=active_week,
                             games=games)
 
-@app.route('/stats')
-def stats():
-    stats = models.TeamStats.query.all()
-    return render_template('stats.html',
-                            stats=stats,
-                            title="Stats")
-
 @app.route('/raw/standings')
 def standings_json():
     teams = models.Teams.query.all()
@@ -118,6 +111,25 @@ def standings_json():
         dataSet.append(team_data)
 
     return json.dumps(dataSet)
+
+@app.route('/raw/stats')
+def stats_json():
+    stats = models.TeamStats.query.all()
+
+    dataSet = []
+
+    for team in stats:
+        team_stats = {'team': team.team_name, 'games': team.games_played, 'totalyards': team.total_yards,
+                    'passingyards': team.passing_yards, 'rushingyards': team.rushing_yards, 'turnovers': team.turnovers,
+                    'qbr': team.qbr}
+        dataSet.append(team_stats)
+
+    return json.dumps(dataSet)
+
+@app.route('/stats')
+def stats():
+    return render_template('stats.html',
+                            title="Stats")
 
 @app.route('/standings')
 def standings():
@@ -133,11 +145,6 @@ def teams():
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def internal_error(error):
-    db.session.rollback()
-    return render_template('500.html'), 500
 
 
 if __name__ == "__main__":
