a702faa9044f87a556cfe3afc81b806d540eb4fc
J2security <josh_pachmayer@yahoo.com>
2017-02-25 14:34:44 -0500

    Updated teamentry to remove the quote from teamnames

--- app/main/teamentry.py
+++ app/main/teamentry.py
@@ -4,19 +4,19 @@ import app.models as models
 
 def teamAdd():
     teams = [
-    {'team_owner': 'Darklord Destro',   'team_city': 'Washington',      'team_name': "'91 Redskins",        'division': 'Navy',      'team_wins': '0', 'team_losses': '0'},
-    {'team_owner': 'tjspeaks',          'team_city': 'Baltimore',       'team_name': "'67 Colts'",          'division': 'Gold',      'team_wins': '0', 'team_losses': '0'},
-    {'team_owner': 'Aquick Assasin',    'team_city': 'Cleveland',       'team_name': "'64 Browns",          'division': 'Navy',      'team_wins': '0', 'team_losses': '0'},
-    {'team_owner': 'MightyRx',          'team_city': 'New Orleans',     'team_name': "'67 Saints",          'division': 'Gold',      'team_wins': '0', 'team_losses': '0'},
-    {'team_owner': 'Dazzo47',           'team_city': 'Chicago',         'team_name': "'67 Bears",           'division': 'Gold',      'team_wins': '0', 'team_losses': '0'},
-    {'team_owner': 'SitnHereFlossin',   'team_city': 'Tampa Bay',       'team_name': "'99 Bucs",            'division': 'Gold',      'team_wins': '0', 'team_losses': '0'},
-    {'team_owner': 'Pcat Magoo',        'team_city': 'Kansas City',     'team_name': "'90 Chiefs",          'division': 'Gold',      'team_wins': '0', 'team_losses': '0'},
-    {'team_owner': 'xRun and Sh00tx',   'team_city': 'Houston',         'team_name': "'93 Oilers",          'division': 'Gold',      'team_wins': '0', 'team_losses': '0'},
-    {'team_owner': 'HeavyHitter55',     'team_city': 'Philadelphia',    'team_name': "'50 Eagles",          'division': 'Navy',      'team_wins': '0', 'team_losses': '0'},
-    {'team_owner': 'RamosLynn',         'team_city': 'Denver',          'team_name': "'98 Broncos",         'division': 'Gold',      'team_wins': '0', 'team_losses': '0'},
-    {'team_owner': 'Dazzo0007',         'team_city': 'Chicago',         'team_name': "'85 Bears",           'division': 'Gold',      'team_wins': '0', 'team_losses': '0'},
-    {'team_owner': 'Bengal1fan2',       'team_city': 'St. Louis',       'team_name': "'00 Rams",            'division': 'Navy',      'team_wins': '0', 'team_losses': '0'},
-    {'team_owner': 'ElectricAggie',     'team_city': 'Dallas',          'team_name': "'93 Cowboys",         'division': 'Navy',      'team_wins': '0', 'team_losses': '0'}
+    {'team_owner': 'Darklord Destro',   'team_city': 'Washington',      'team_name': "91 Redskins",        'division': 'Navy',      'team_wins': '0', 'team_losses': '0'},
+    {'team_owner': 'tjspeaks',          'team_city': 'Baltimore',       'team_name': "67 Colts'",          'division': 'Gold',      'team_wins': '0', 'team_losses': '0'},
+    {'team_owner': 'Aquick Assasin',    'team_city': 'Cleveland',       'team_name': "64 Browns",          'division': 'Navy',      'team_wins': '0', 'team_losses': '0'},
+    {'team_owner': 'MightyRx',          'team_city': 'New Orleans',     'team_name': "67 Saints",          'division': 'Gold',      'team_wins': '0', 'team_losses': '0'},
+    {'team_owner': 'Dazzo47',           'team_city': 'Chicago',         'team_name': "67 Bears",           'division': 'Gold',      'team_wins': '0', 'team_losses': '0'},
+    {'team_owner': 'SitnHereFlossin',   'team_city': 'Tampa Bay',       'team_name': "99 Bucs",            'division': 'Gold',      'team_wins': '0', 'team_losses': '0'},
+    {'team_owner': 'Pcat Magoo',        'team_city': 'Kansas City',     'team_name': "90 Chiefs",          'division': 'Gold',      'team_wins': '0', 'team_losses': '0'},
+    {'team_owner': 'xRun and Sh00tx',   'team_city': 'Houston',         'team_name': "93 Oilers",          'division': 'Gold',      'team_wins': '0', 'team_losses': '0'},
+    {'team_owner': 'HeavyHitter55',     'team_city': 'Philadelphia',    'team_name': "50 Eagles",          'division': 'Navy',      'team_wins': '0', 'team_losses': '0'},
+    {'team_owner': 'RamosLynn',         'team_city': 'Denver',          'team_name': "98 Broncos",         'division': 'Gold',      'team_wins': '0', 'team_losses': '0'},
+    {'team_owner': 'Dazzo0007',         'team_city': 'Chicago',         'team_name': "85 Bears",           'division': 'Gold',      'team_wins': '0', 'team_losses': '0'},
+    {'team_owner': 'Bengal1fan2',       'team_city': 'St. Louis',       'team_name': "00 Rams",            'division': 'Navy',      'team_wins': '0', 'team_losses': '0'},
+    {'team_owner': 'ElectricAggie',     'team_city': 'Dallas',          'team_name': "93 Cowboys",         'division': 'Navy',      'team_wins': '0', 'team_losses': '0'}
     ]
 
     for team in teams:
